a8b57424e03c61ac7a249ed24188f7f8d75c8987
antara-walid <Antara.walid.27@gmail.com>
2022-10-18 19:41:11 +0100

    first version of Stack class

--- src/main/java/org/example/Stack.java
+++ src/main/java/org/example/Stack.java
@@ -1,14 +1,14 @@
 package org.example;
 
-public class Stack {
+public class Stack<T> {
 
     // stackNode
-    private static class StackNode {
-        private int data;
+    private static class StackNode<T> {
+        private T data;
         private StackNode next;
 
         // constructor
-        private StackNode(int data)
+        private StackNode(T data)
         {
             this.data= data;
         }
@@ -24,7 +24,32 @@ public class Stack {
     }
 
     // 2.peek()
+    public T peek()
+    {
+        // check if stack is empty in this case throw exception
+        if(top == null )
+        {
+            throw new RuntimeException("Stack is empty");
+        }
+
+        return (T) top.data;
+
+    }
     // 3.push()
+
+    public void push(T data)
+    {
+        StackNode newNode = new StackNode(data);
+        newNode.next = top;
+        top = newNode;
+    }
     // 4.pop()
+
+    public T pop()
+    {
+        T data = (T) top.data;
+        top = top.next; // garbage collector will remove the unlinked node
+        return data;
+    }
 
 }
